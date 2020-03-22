097275d8033d809b168e18f06028c46d95c3ba2d
Manuel Wegria <abstract.s33d@gmail.com>
2020-03-22 19:34:39 +0100

    update post install

--- post-install.sh
+++ post-install.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/bash
-bundle install
-make shell cmd="rm -f config/credentials.yml; EDITOR='mate --wait' bin/rails credentials:edit"
+# by default config/credentials.yml.enc is not in .gitignore
+make shell cmd="rm -f config/credentials.yml.enc || true"
+# generate secret.key & config/credentials.yml.enc
+make shell cmd="EDITOR='mate --wait' bin/rails credentials:edit"
+# create and seed database
 make shell cmd="rake db:create && rake db:migrate"
+# link .env
+ln -s /home/ec2-user/envs/global_api.env /home/ec2-user/projects/global_api/.env || true
+# restart service
+make service
