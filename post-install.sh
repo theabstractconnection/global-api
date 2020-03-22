e73be0555b1a25a207a477cee99f9f1970098788
Manuel Wegria <abstract.s33d@gmail.com>
2020-03-22 19:26:28 +0100

    remove master.key && credentials.yml + add them to gitignore

--- post-install.sh
+++ post-install.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/bash
-rake secret
+bundle install
+make shell cmd="rm -f config/credentials.yml; EDITOR='mate --wait' bin/rails credentials:edit"
 make shell cmd="rake db:create && rake db:migrate"
