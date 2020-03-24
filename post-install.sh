d38d7eed3c0604f8ddecbc41e1b56a857e79bc1b
Manuel Wegria <abstract.s33d@gmail.com>
2020-03-24 18:36:48 +0100

    update shebang to #!/usr/bin/env bash

--- post-install.sh
+++ post-install.sh
@@ -1,9 +1,9 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
+# by default config/credentials.yml.enc is not in .gitignore
+make shell cmd="rm -f config/credentials.yml.enc || true"
 # generate secret.key & config/credentials.yml.enc
 make shell cmd="EDITOR='mate --wait' bin/rails credentials:edit"
 # create and seed database
-make shell cmd="rake db:create && rake db:migrate"
-# link .env
-ln -s /home/ec2-user/envs/global_api.env /home/ec2-user/projects/global_api/.env || true
+make shell cmd="rake db:create && rake db:migrate & rake db:seed"
 # restart service
 make service
