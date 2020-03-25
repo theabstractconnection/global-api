edde034a977e994dd891e98c8506beb9ba87b346
Manuel Wegria <abstract.s33d@gmail.com>
2020-03-25 18:41:50 +0100

    split rails db create migrate seed for initial setup

--- post-install.sh
+++ post-install.sh
@@ -3,7 +3,9 @@
 make shell cmd="rm -f config/credentials.yml.enc || true"
 # generate secret.key & config/credentials.yml.enc
 make shell cmd="EDITOR='mate --wait' bin/rails credentials:edit"
-# create and seed database
-make shell cmd="rake db:create && rake db:migrate & rake db:seed"
+# create, migrate and seed database
+make shell cmd="rake db:create"
+make shell cmd="rake db:migrate"
+make shell cmd="rake db:seed"
 # restart service
 make service
